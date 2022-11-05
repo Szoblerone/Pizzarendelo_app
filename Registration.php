867cea5c1d152f812586c0b44b590054477a7b1e
Birodalmix <gudmann.benjamin@gmail.com>
2022-11-05 15:07:44 +0100

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -1,24 +1,24 @@
 <?php $pagename="Regisztráció"; ?>
 <?php require_once("parts/htmlTop.php");?>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <input type="text" name="username" id="username"><br>
-        <label for="username">felhasználónév: </label><br><br>
-        <input type="password" name="password" id="password"><br>
-        <input type="password" name="passwordre" id="passwordre"><br>
-        <input type="email" name="email" id="email"><br>
-        <input type="text" name="firstname" id="firstname"><br>
-        <input type="text" name="lastname" id="lastname"><br>
+        <input type="text" name="username" id="username" placeholder="felhasználónév"><br>
+        <input type="password" name="password" id="password" placeholder="jelszó"><br>
+        <input type="password" name="passwordre" id="passwordre" placeholder="jelszó mégegyszer"><br>
+        <input type="email" name="email" id="email" placeholder="email"><br>
+        <input type="text" name="firstname" id="firstname" placeholder="vezetéknév"><br>
+        <input type="text" name="lastname" id="lastname" placeholder="keresztnév"><br>
         Nem:
         <input type="radio" name="gender" value="female" checked>nő
         <input type="radio" name="gender" value="male">férfi
-        <input type="radio" name="gender" value="other">egyéb
+        <input type="radio" name="gender" value="other">egyéb<br>
 
-        <input type="number" name="zipcode" id="zipcode"><br>
-        <input type="text" name="city" id="city"><br>
-        <input type="text" name="street" id="street"><br>
-        <input type="tel" name="phone" id="phone"><br>
+        <input type="date" name="birthdate" id="birthdate"><br><br>
+        <input type="number" name="zipcode" id="zipcode" placeholder="irányítószám"><br>
+        <input type="text" name="city" id="city" placeholder="város"><br>
+        <input type="text" name="street" id="street" placeholder="utca és házszám"><br>
+        <input type="tel" name="phone" id="phone" placeholder="telefonszám"><br>
 
-        <input type="submit" value="submit" name="newuser"><br>
+        <input type="submit" value="Elküld" name="newuser"><br>
     </form>
 <?php 
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
