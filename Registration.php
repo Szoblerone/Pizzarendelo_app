cfb31d5aab2f91bd83ac55c0873ac55f77ff1a18
Birodalmix <gudmann.benjamin@gmail.com>
2022-11-05 14:54:36 +0100

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -1,3 +1,4 @@
+<?php $pagename="Regisztráció"; ?>
 <?php require_once("parts/htmlTop.php");?>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
         <input type="text" name="username" id="username"><br>
