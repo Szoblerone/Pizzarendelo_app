2a6e0c63404c88ffb3cfe54bff385dbfda0c0d24
Szoblerone <martin99komuves@gmail.com>
2022-11-05 12:28:04 +0100

    a

--- Index.php
+++ Index.php
@@ -1,6 +1,7 @@
 <?php $pagename= "Főoldal" require_once ("htmlTop.php");?>
+<?php require_once ("menu.php")?>
 
-
+<input type="text" id="search-input" placeholder="Keresés..." class="stickyHeader">
 
 
 
