7710fb86b8a028359d5d614eaa065e368aadca85
Birodalmix <gudmann.benjamin@gmail.com>
2022-11-05 12:16:40 +0100

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -1,23 +1,26 @@
+<?php $pagename="Regisztráció";?>
 <?php require_once("parts/htmlTop.php");?>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <input type="text" name="username" id="username"><br>
-        <label for="username">felhasználónév: </label><br><br>
-        <input type="password" name="password" id="password"><br>
-        <input type="password" name="passwordre" id="passwordre"><br>
-        <input type="email" name="email" id="email"><br>
-        <input type="text" name="firstname" id="firstname"><br>
-        <input type="text" name="lastname" id="lastname"><br>
+        <input type="text" name="username" id="username" placeholder="felhasználónév"><br>
+        <input type="password" name="password" id="password" placeholder="jelszó"><br>
+        <input type="password" name="passwordre" id="passwordre" placeholder="jelszó még egyszer"><br>
+        <input type="email" name="email" id="email" placeholder="email cím"><br>
+        <input type="text" name="firstname" id="firstname" placeholder="vezetéknév"><br>
+        <input type="text" name="lastname" id="lastname" placeholder="keresztnév"><br>
         Nem:
         <input type="radio" name="gender" value="female">nő
         <input type="radio" name="gender" value="male">férfi
-        <input type="radio" name="gender" value="other">egyéb
+        <input type="radio" name="gender" value="other">egyéb<br><br>
 
-        <input type="number" name="zipcode" id="zipcode"><br>
-        <input type="text" name="city" id="city"><br>
-        <input type="text" name="street" id="street"><br>
-        <input type="tel" name="phone" id="phone"><br>
+        <label for="birthdate">Adja meg a születési évét:</label>
+        <input type="date" name="birthdate" id="birthdate" min="1900-01-01"><br><br>
 
-        <input type="submit" value="submit" name="newuser"><br>
+        <input type="number" name="zipcode" id="zipcode" placeholder="irányító szám"><br>
+        <input type="text" name="city" id="city" placeholder="város"><br>
+        <input type="text" name="street" id="street" placeholder="utca és ház szám"><br>
+        <input type="tel" name="phone" id="phone" placeholder="telefonszám"><br>
+
+        <input type="submit" value="Elküld" name="newuser"><br>
     </form>
 <?php require("DatabaseParts/Userdata_server_connect.php");?>
 
