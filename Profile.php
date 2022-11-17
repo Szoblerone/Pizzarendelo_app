3e8cbb3526e9b51abe5a847b0aec05db91beb2ac
Birodalmix <gudmann.benjamin@gmail.com>
2022-11-17 17:48:55 +0100

    profil end??

--- Profile.php
+++ Profile.php
@@ -6,6 +6,8 @@ require_once("parts/menu.php");
 require("DatabaseParts/Userdata_server_connect.php");
 
 ?>
+
+
 <style type="text/css">
 .tg  {border-collapse:collapse;border-color:#C44D58;border-spacing:0;margin:50px auto;}
 .tg td{background-color:#F9CDAD;border-color:#C44D58;border-style:solid;border-width:0px;color:#002b36;
@@ -25,12 +27,38 @@ require("DatabaseParts/Userdata_server_connect.php");
   <tr>
     <td class="tg-0pky">felhasználónév</td>
     <td class="tg-0lax">Sanyika</td>
-    <td class="tg-0pky">módosítás</td>
+    <td class="tg-0pky"><!-- Button to open the modal login form -->
+     <button class="change" onclick="document.getElementById('id02').style.display='block'">modosítás</button>
+     <div id="id02" class="modal">
+     <span onclick="document.getElementById('id02').style.display='none'"
+     class="close" title="Close Modal">&times;</span>
+     <form class="modal-content animate" action="/action_page.php">
+         <div class="container" style="background-color: #Ba181b;">
+         <input type="text" name="username" placeholder="Felhasználónév" required><br>
+         <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Mégse</button>
+             <button type="submit" name="user">Bejelentkezés</button>
+         </div>
+     </form>
+     </div>
+    </td>
   </tr>
   <tr>
     <td class="tg-0pky">jelszó</td>
     <td class="tg-0lax"></td>
-    <td class="tg-0pky">módosítás</td>
+    <td class="tg-0pky">     
+      <button class="change" onclick="document.getElementById('id03').style.display='block'">modosítás</button>
+     <div id="id03" class="modal">
+     <span onclick="document.getElementById('id03').style.display='none'"
+     class="close" title="Close Modal">&times;</span>
+     <form class="modal-content animate" action="/action_page.php">
+         <div class="container" style="background-color: #Ba181b;">
+         <input type="text" name="username" placeholder="Felhasználónév" required><br>
+         <input type="password" name="password" placeholder="Jelszó" required>
+         <button type="button" onclick="document.getElementById('id03').style.display='none'" class="cancelbtn">Mégse</button>
+             <button type="submit" name="user">Bejelentkezés</button>
+         </div>
+     </form>
+     </div></td>
   </tr>
   <tr>
     <td class="tg-0pky">email</td>
