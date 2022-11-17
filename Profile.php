1e0d0e3e0db62b3be6946cb62c292f160950ec57
Birodalmix <gudmann.benjamin@gmail.com>
2022-11-17 14:52:16 +0100

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -3,7 +3,7 @@ session_start();
 $pagename="Profil";
 require_once("parts/htmlTop.php");
 require_once("parts/menu.php");
-
+required("DatabaseParts\Userdata_server_connect.php");
 
 require_once("parts/htmlBottom.php");
 ?>
