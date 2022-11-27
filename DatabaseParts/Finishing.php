3b8e3db1fdbf238f3b0a138bdaa48e94f3cfed1b
Birodalmix <gudmann.benjamin@gmail.com>
2022-11-27 17:20:28 +0100

    userdata

--- DatabaseParts/Finishing.php
+++ DatabaseParts/Finishing.php
@@ -1,5 +1,16 @@
 <?php
-require("DatabaseParts/Userdata_server_connect.php");
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=userdata","root","");	// adatbázis kapcsolat PDO-val
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);		// kivétel alapú hibakezelés beállítása
+}
+catch (PDOException $e){
+    echo "<p class='error'>Adatbázis kapcsolódási hiba: ".$e->getMessage()."</p>\n";		// PDO által dobott adatbázis hiba
+    die;
+}
+catch (Exception $e){
+    echo "<p class='error'>Hiba: ".$e->getMessage()."</p>\n";		// valamilyen más hiba
+    die;
+}
 try {
     $ready = 1;
     $sqlu = "UPDATE orders SET ready=:ready WHERE orderid = :orderidq"; // paraméterezett lekérdezés
